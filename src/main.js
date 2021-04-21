3134096707d5ad5066e212504b1b72b9bc8e43e6
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-22 02:01:25 +0530

    feat: ability to find luckiest day and unluckiest day

--- src/main.js
+++ src/main.js
@@ -16,27 +16,58 @@ dayCount = 0;
 
 for (let day = 0; day < 20; day++) {
   dayCount++;
-  let dayStack = 0;
+  var roundCount = 0;
   let wonCount = 0;
   let lostCount = 0;
 
+  let dayStack = 0;
+
   while (dayStack < 50 && dayStack > -50) {
+    roundCount++;
     let result = betResult();
     if (result == 1) wonCount++;
     if (result == -1) lostCount++;
     dayStack = dayStack + result;
   }
   monthResult.push(dayStack);
-  if (dayStack == 50) {
-    wonCountMap.set(wonCount, dayStack);
+  if (wonCount > lostCount) {
+    wonCountMap.set(dayCount, wonCount);
   } else {
-    lostCountMap.set(lostCount, dayStack);
+    lostCountMap.set(dayCount, lostCount);
   }
 }
+
+//For Most Win
+wonCountArray = [];
+wonCountMap.forEach((value, keys) => {
+  wonCountArray.push(value);
+});
+let mostWin = Math.max.apply(Math, wonCountArray);
+
+wonCountMap.forEach((value, keys) => {
+  if (value == mostWin) {
+    console.log(`The Day When Gambler Win The Most is ${keys}`);
+  }
+});
+
 console.log(wonCountMap);
+
+//For Most Lost 
+lostCountArray = [];
+lostCountMap.forEach((value, keys) => {
+  lostCountArray.push(value);
+});
+let mostLost = Math.max.apply(Math, lostCountArray);
+
+lostCountMap.forEach((value, keys) => {
+  if (value == mostLost) {
+    console.log(`The Day When Gambler Lost The Most is ${keys}`);
+  }
+});
 
 console.log(lostCountMap);
 
+//For Calculate Final Stack at monthEnd 
 finalStack = monthResult.reduce((a, b) => a + b, 0);
 
 console.log(`After ${dayCount} Days of Palying Final Amount is ${finalStack}`);
