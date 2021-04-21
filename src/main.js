e5556ba57314af42ff374e58e9e42a192c4a81b5
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-21 21:39:36 +0530

    feat: check gambler win or loss

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 const totelAmount = 100;
 const betAmount = 1;
+
+let betCount = 0;
+
+bet = () => {
+  randomBet = Math.floor(Math.random() * 10) % 2;
+  betCount++
+
+  switch(randomBet){
+        case 1:
+            console.log("Bet Win..");
+            break;
+        case 0:
+            console.log("Bet Loss..");
+            break;
+        default:
+            console.log("No Result");
+            
+  }
+  console.log("bet count: " + betCount);
+  return betCount;
+};
+
+bet();
