282f14f7bf5f2f602b00c5cd23196de73754e675
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-22 00:44:01 +0530

    feat: ability to count number of days won or lost

--- src/main.js
+++ src/main.js
@@ -1,34 +1,42 @@
-const totelStack = 100;
-
 betResult = () => {
-randomBet = Math.floor(Math.random () * 10) % 2;
-  switch(randomBet){
-        case 1:
-            return 1
-        case 0:
-            return -1
+  randomBet = Math.floor(Math.random() * 10) % 2;
+  switch (randomBet) {
+    case 1:
+      return 1;
+    case 0:
+      return -1;
   }
 };
- 
-let result = [];
-dayCount = 0
 
-for(let i=0; i<20; i++){
-    dayCount++
-    var dayStack=0;
-    while (dayStack < 50 && dayStack > -50){
-        dayStack = dayStack + betResult()
-    }
-    result.push(dayStack);
+let monthResult = [];
+let lostCountMap = new Map();
+let wonCountMap = new Map();
+
+dayCount = 0;
+
+for (let day = 0; day < 20; day++) {
+  dayCount++;
+  let dayStack = 0;
+  let wonCount = 0;
+  let lostCount = 0;
+
+  while (dayStack < 50 && dayStack > -50) {
+    let result = betResult();
+    if (result == 1) wonCount++;
+    if (result == -1) lostCount++;
+    dayStack = dayStack + result;
+  }
+  monthResult.push(dayStack);
+  if (dayStack == 50) {
+    wonCountMap.set(wonCount, dayStack);
+  } else {
+    lostCountMap.set(lostCount, dayStack);
+  }
 }
+console.log(wonCountMap);
 
-console.log(result);
+console.log(lostCountMap);
 
-finalStack = result.reduce((a,b)=> a+b, 0)
+finalStack = monthResult.reduce((a, b) => a + b, 0);
 
 console.log(`After ${dayCount} Days of Palying Final Amount is ${finalStack}`);
-
-
-
-
-
